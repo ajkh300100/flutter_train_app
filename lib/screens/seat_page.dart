702c82bf3183075cfea21f9be206adcaadb68a87
진규혁 <ajkh300100@naver.com>
2025-03-31 14:04:23 +0900

    주석 수정

--- lib/screens/seat_page.dart
+++ lib/screens/seat_page.dart
@@ -16,6 +16,41 @@ class _SeatPageState extends State<SeatPage> {
     (_) => List.generate(4, (_) => false),
   );
 
+  // 선택된 좌석을 기록할 리스트
+  List<String> selectedSeats = [];
+
+  void _updateSelectedSeats(int row, int col) {
+    // 좌석 번호 추가 또는 제거
+    String seatLabel = _getSeatLabel(row, col);
+    setState(() {
+      if (seatStatus[row][col]) {
+        selectedSeats.add(seatLabel);
+      } else {
+        selectedSeats.remove(seatLabel);
+      }
+    });
+  }
+
+  // 좌석 번호 생성
+  String _getSeatLabel(int row, int col) {
+    String letter = '';
+    switch (col) {
+      case 0:
+        letter = 'A';
+        break;
+      case 1:
+        letter = 'B';
+        break;
+      case 2:
+        letter = 'C';
+        break;
+      case 3:
+        letter = 'D';
+        break;
+    }
+    return '$letter-${row + 1}';
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -41,6 +76,41 @@ class _SeatPageState extends State<SeatPage> {
               color: Colors.purple,
             ),
             SizedBox(height: 20),
+
+            // 좌석 상태 안내 레이블 추가
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  '좌석 선택 상태:',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(width: 10),
+                Container(
+                  width: 20,
+                  height: 20,
+                  color: Colors.grey[300],
+                  child: Center(child: Text(' ')),
+                ),
+                Text(
+                  ' 미선택',
+                  style: TextStyle(fontSize: 16),
+                ),
+                SizedBox(width: 20),
+                Container(
+                  width: 20,
+                  height: 20,
+                  color: Colors.purple,
+                  child: Center(child: Text(' ')),
+                ),
+                Text(
+                  ' 선택됨',
+                  style: TextStyle(fontSize: 16),
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+
             Expanded(
               child: Column(
                 children: [
@@ -48,7 +118,7 @@ class _SeatPageState extends State<SeatPage> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      // A와 B 위에 정확히 배치
+                      // A와 B 위에 배치
                       Container(
                         width: 50,
                         child: Center(
@@ -68,7 +138,7 @@ class _SeatPageState extends State<SeatPage> {
                         ),
                       ),
                       SizedBox(width: 80), // B와 C 사이 간격 추가
-                      // C와 D 위에 정확히 배치
+                      // C와 D 위에 배치
                       Container(
                         width: 50,
                         child: Center(
@@ -103,6 +173,7 @@ class _SeatPageState extends State<SeatPage> {
                               onTap: () {
                                 setState(() {
                                   seatStatus[rowIndex][0] = !seatStatus[rowIndex][0];
+                                  _updateSelectedSeats(rowIndex, 0);
                                 });
                               },
                               child: Container(
@@ -128,6 +199,7 @@ class _SeatPageState extends State<SeatPage> {
                               onTap: () {
                                 setState(() {
                                   seatStatus[rowIndex][1] = !seatStatus[rowIndex][1];
+                                  _updateSelectedSeats(rowIndex, 1);
                                 });
                               },
                               child: Container(
@@ -148,7 +220,7 @@ class _SeatPageState extends State<SeatPage> {
                                 ),
                               ),
                             ),
-                            // 숫자 (가운데)
+                            // 좌석 숫자 배치 (가운데)
                             Container(
                               width: 50,
                               height: 50,
@@ -163,6 +235,7 @@ class _SeatPageState extends State<SeatPage> {
                               onTap: () {
                                 setState(() {
                                   seatStatus[rowIndex][2] = !seatStatus[rowIndex][2];
+                                  _updateSelectedSeats(rowIndex, 2);
                                 });
                               },
                               child: Container(
@@ -188,6 +261,7 @@ class _SeatPageState extends State<SeatPage> {
                               onTap: () {
                                 setState(() {
                                   seatStatus[rowIndex][3] = !seatStatus[rowIndex][3];
+                                  _updateSelectedSeats(rowIndex, 3);
                                 });
                               },
                               child: Container(
@@ -211,6 +285,48 @@ class _SeatPageState extends State<SeatPage> {
                           ],
                         );
                       },
+                    ),
+                  ),
+                  ElevatedButton(
+                    onPressed: selectedSeats.isNotEmpty
+                        ? () {
+                            showDialog(
+                              context: context,
+                              builder: (context) => AlertDialog(
+                                title: Text('선택된 좌석'),
+                                content: Text('선택한 좌석: ${selectedSeats.join(', ')}'),
+                                actions: [
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context); // 다이얼로그 닫기
+                                    },
+                                    child: Text('취소'),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context); // 다이얼로그 닫기
+                                      Navigator.pop(context); // HomePage로 돌아가기
+                                    },
+                                    child: Text('확인'),
+                                  ),
+                                ],
+                              ),
+                            );
+                          }
+                        : null,
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.purple),
+                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20),
+                      )),
+                    ),
+                    child: Text(
+                      '예매하기',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
                 ],
