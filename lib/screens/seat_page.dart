91467194c6f779c99990241c72dd47e25371ff56
진규혁 <ajkh300100@naver.com>
2025-03-31 13:55:50 +0900

    좌석 상태 안내 레이블 및 예매 기능 추가

--- lib/screens/seat_page.dart
+++ lib/screens/seat_page.dart
@@ -4,7 +4,7 @@ class SeatPage extends StatefulWidget {
   final String departure;
   final String arrival;
 
-  SeatPage({required this.departure, required this.arrival, required Key key}) : super(key: key);
+  SeatPage({required this.departure, required this.arrival, required Key key});
 
   @override
   _SeatPageState createState() => _SeatPageState();
@@ -12,7 +12,7 @@ class SeatPage extends StatefulWidget {
 
 class _SeatPageState extends State<SeatPage> {
   List<List<bool>> seatStatus = List.generate(
-    20,
+    10,
     (_) => List.generate(4, (_) => false),
   );
 
@@ -42,70 +42,178 @@ class _SeatPageState extends State<SeatPage> {
             ),
             SizedBox(height: 20),
             Expanded(
-              child: ListView.builder(
-                itemCount: 20,
-                itemBuilder: (context, rowIndex) {
-                  return Row(
+              child: Column(
+                children: [
+                  // A와 B는 왼쪽 열에 배치, C와 D는 오른쪽 열에 배치
+                  Row(
                     mainAxisAlignment: MainAxisAlignment.center,
-                    children: List.generate(4, (colIndex) {
-                      return GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            seatStatus[rowIndex][colIndex] =
-                                !seatStatus[rowIndex][colIndex]; // 좌석 상태 변경
-                          });
-                        },
-                        child: Container(
-                          width: 50,
-                          height: 50,
-                          margin: EdgeInsets.symmetric(horizontal: 4, vertical: 8),
-                          decoration: BoxDecoration(
-                            color: seatStatus[rowIndex][colIndex]
-                                ? Colors.purple
-                                : Colors.grey[300],
-                            borderRadius: BorderRadius.circular(8),
+                    children: [
+                      // A와 B 위에 정확히 배치
+                      Container(
+                        width: 50,
+                        child: Center(
+                          child: Text(
+                            'A',
+                            style: TextStyle(fontSize: 18),
                           ),
                         ),
-                      );
-                    }),
-                  );
-                },
-              ),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: seatStatus.any((row) => row.any((seat) => seat))
-                  ? () {
-                      showDialog(
-                        context: context,
-                        builder: (context) => AlertDialog(
-                          title: Text('예매 완료'),
-                          content: Text('좌석 예매가 완료되었습니다.'),
-                          actions: [
-                            TextButton(
-                              onPressed: () {
-                                Navigator.pop(context);
+                      ),
+                      Container(
+                        width: 50,
+                        child: Center(
+                          child: Text(
+                            'B',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ),
+                      SizedBox(width: 80), // B와 C 사이 간격 추가
+                      // C와 D 위에 정확히 배치
+                      Container(
+                        width: 50,
+                        child: Center(
+                          child: Text(
+                            'C',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 50,
+                        child: Center(
+                          child: Text(
+                            'D',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 8), // ABCD와 좌석 간의 세로 간격 추가
+                  // 좌석 아래에 번호 표시
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: 10,
+                      itemBuilder: (context, rowIndex) {
+                        return Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            // 좌석 번호 A (왼쪽)
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  seatStatus[rowIndex][0] = !seatStatus[rowIndex][0];
+                                });
                               },
-                              child: Text('확인'),
+                              child: Container(
+                                width: 50,
+                                height: 50,
+                                margin: EdgeInsets.symmetric(horizontal: 4),
+                                decoration: BoxDecoration(
+                                  color: seatStatus[rowIndex][0]
+                                      ? Colors.purple
+                                      : Colors.grey[300],
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    '',
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            // 좌석 번호 B (왼쪽)
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  seatStatus[rowIndex][1] = !seatStatus[rowIndex][1];
+                                });
+                              },
+                              child: Container(
+                                width: 50,
+                                height: 50,
+                                margin: EdgeInsets.symmetric(horizontal: 4),
+                                decoration: BoxDecoration(
+                                  color: seatStatus[rowIndex][1]
+                                      ? Colors.purple
+                                      : Colors.grey[300],
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    '',
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            // 숫자 (가운데)
+                            Container(
+                              width: 50,
+                              height: 50,
+                              alignment: Alignment.center,
+                              child: Text(
+                                '${rowIndex + 1}',
+                                style: TextStyle(fontSize: 18),
+                              ),
+                            ),
+                            // 좌석 번호 C (오른쪽)
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  seatStatus[rowIndex][2] = !seatStatus[rowIndex][2];
+                                });
+                              },
+                              child: Container(
+                                width: 50,
+                                height: 50,
+                                margin: EdgeInsets.symmetric(horizontal: 4),
+                                decoration: BoxDecoration(
+                                  color: seatStatus[rowIndex][2]
+                                      ? Colors.purple
+                                      : Colors.grey[300],
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    '',
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            // 좌석 번호 D (오른쪽)
+                            GestureDetector(
+                              onTap: () {
+                                setState(() {
+                                  seatStatus[rowIndex][3] = !seatStatus[rowIndex][3];
+                                });
+                              },
+                              child: Container(
+                                width: 50,
+                                height: 50,
+                                margin: EdgeInsets.symmetric(horizontal: 4),
+                                decoration: BoxDecoration(
+                                  color: seatStatus[rowIndex][3]
+                                      ? Colors.purple
+                                      : Colors.grey[300],
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    '',
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
+                              ),
                             ),
                           ],
-                        ),
-                      );
-                    }
-                  : null,
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.purple),
-                shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(20),
-                )),
-              ),
-              child: Text(
-                '예매하기',
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
